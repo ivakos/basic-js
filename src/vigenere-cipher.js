e7ad39dae4ba3274b9a298d8609311e92a8526c6
Ivan Kasenka <kasenka.ivan@gmail.com>
2023-04-18 02:36:36 +0300

    feat: add vigenere-cipher solution

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -21,13 +21,79 @@ const { NotImplementedError } = require('../extensions/index.js');
  */
 class VigenereCipheringMachine {
 
-  encrypt() {
+  constructor(front = true) {
+    this.front = front;
+  }
 
+  alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
+  encrypt(message, key) {
+    let result = '';
+
+    if (!message || !key) throw new Error('Incorrect arguments!');
+
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+
+    let countKeyIndex = 0;
+
+    for (let i = 0; i < message.length; i++) {
+
+      if (countKeyIndex > key.length - 1) countKeyIndex = 0;
+      if (!this.alphabet.includes(message[i])) {
+        result += message[i];
+        continue;
+      }
+
+      let letterIndex = this.alphabet.indexOf(message[i]) + this.alphabet.indexOf(key[countKeyIndex]);
+
+      if (this.alphabet.length <= letterIndex) {
+        letterIndex -= this.alphabet.length;
+      }
+
+      result += this.alphabet[letterIndex];
+      countKeyIndex++;
+    }
+
+    if (this.front == false) result = result.split('').reverse().join('')
+
+    return result
   }
-  decrypt() {
-ss
+
+
+  decrypt(encryptedMessage, key) {
+    let result = '';
+
+    if (!encryptedMessage || !key) throw new Error('Incorrect arguments!');
+
+    encryptedMessage = encryptedMessage.toUpperCase();
+    key = key.toUpperCase();
+
+    let countKeyIndex = 0;
+
+    for (let i = 0; i < encryptedMessage.length; i++) {
+
+      if (countKeyIndex > key.length - 1) countKeyIndex = 0;
+      if (!this.alphabet.includes(encryptedMessage[i])) {
+        result += encryptedMessage[i];
+        continue;
+      }
+
+      let letterIndex = this.alphabet.indexOf(encryptedMessage[i]) - this.alphabet.indexOf(key[countKeyIndex]);
+
+      if (letterIndex < 0) {
+        letterIndex = this.alphabet.length - -letterIndex;
+      }
+
+      result += this.alphabet[letterIndex];
+      countKeyIndex++;
+    }
+
+    if (this.front == false) result = result.split('').reverse().join('')
+
+    return result
   }
-  
+
 }
 
 module.exports = {
